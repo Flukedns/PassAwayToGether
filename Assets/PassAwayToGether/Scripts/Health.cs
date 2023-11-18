402e691db6cee46ba59226b9f394cadfb2bbffb0
Namnile <72727840+NxmNile@users.noreply.github.com>
2023-11-18 18:20:49 +0700

    ADD  model/ Pause Menu

---
+++ Assets/PassAwayToGether/Scripts/Health.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{   
+    [SerializeField] private GameObject[] Hearts;
+    public int HP;
+    void Start()
+    {
+        HP = 5;
+    }
+
+    public void SetHP()
+    {
+        if (HP == 1)
+        {
+            Hearts[0].SetActive(true);
+            Hearts[1].SetActive(false);
+            Hearts[2].SetActive(false);
+            Hearts[3].SetActive(false);
+            Hearts[4].SetActive(false);
+        }
+        else if (HP == 2)
+        {
+            Hearts[0].SetActive(true);
+            Hearts[1].SetActive(true);
+            Hearts[2].SetActive(false);
+            Hearts[3].SetActive(false);
+            Hearts[4].SetActive(false);
+        }
+        else if (HP == 3)
+        {
+            Hearts[0].SetActive(true);
+            Hearts[1].SetActive(true);
+            Hearts[2].SetActive(true);
+            Hearts[3].SetActive(false);
+            Hearts[4].SetActive(false);
+        }
+        else if (HP == 4)
+        {
+            Hearts[0].SetActive(true);
+            Hearts[1].SetActive(true);
+            Hearts[2].SetActive(true);
+            Hearts[3].SetActive(true);
+            Hearts[4].SetActive(false);
+        }
+        else if (HP == 5)
+        {
+            Hearts[0].SetActive(true);
+            Hearts[1].SetActive(true);
+            Hearts[2].SetActive(true);
+            Hearts[3].SetActive(true);
+            Hearts[4].SetActive(true);
+        }
+    }
+    public void AddHP(int heal)
+    {
+        HP += heal;
+        SetHP();
+    }
+    
+
+   
+}
