4072a3770dc38b3645999bde1a94335a6fe44ff3
Ekulf23 <danusorn.chan@bumail.net>
2023-11-24 13:35:48 +0700

    AiManager
    AiManager

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Transform[] spawnPoint;
+    public GameObject enemyPrefab;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnNewEnemy();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnNewEnemy()
     {
+<<<<<<< Updated upstream:Assets/NewBehaviourScript.cs
         //if(OVRInput.GetDown(OVRInput.RawButton.))
+=======
+        Instantiate(enemyPrefab, spawnPoint[0].transform.position, Quaternion.identity);
+>>>>>>> Stashed changes:Assets/Scripts/EnemySpawn.cs
     }
 }
